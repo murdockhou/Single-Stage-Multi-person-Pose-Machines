513ab92bf1714a6b3e908c6783d80043ab811fa3
murdockhou <murdockhou@gmail.com>
2019-11-08 16:04:26 +0800

    update model_val.py

--- tools/model_val.py
+++ tools/model_val.py
@@ -5,6 +5,7 @@ import numpy as np
 import json
 import time
 
+
 from nets.spm_model import SpmModel
 from dataset.dataset import get_dataset
 from config.center_config import center_config as params
@@ -15,9 +16,10 @@ netH = params['height']
 netW = params['width']
 score = 0.6
 dist = 20
-ckpt_path = '/home/hsw/server/ckpt-74/ckpt-80'
-
+# ckpt_path = '/home/hsw/server/ckpt-74/ckpt-84/ckpt-87'
+ckpt_path = '/home/hsw/server/multi_pose/spm/keras/ckpt_'
 # params['val_json_file'] = 'jsons/val_500.json'
+ckpts = [1, 2, 3, 4]
 
 @tf.function
 def infer(model, inputs):
@@ -42,66 +44,54 @@ if __name__ == '__main__':
     inputs = tf.keras.Input(shape=(netH, netW, 3), name='modelInput')
     outputs = SpmModel(inputs, 14, is_training=False)
     model = tf.keras.Model(inputs, outputs)
-    ckpt = tf.train.Checkpoint(net=model)
 
-    ckpt.restore(ckpt_path)
+    # ckpt = tf.train.Checkpoint(net=model)
+    # ckpt.restore(ckpt_path).assert_existing_objects_matched()
 
-    val_dataset = get_dataset(mode='val')
-    predictions = []
+    for ckpt in ckpts:
+        model.load_weights(ckpt_path+str(ckpt))
 
-    for step, (imgids, heights, widths, imgs) in enumerate(val_dataset):
-        s = time.time()
-        imgs = tf.reshape(imgs, shape=(-1, netH, netW, 3))
-        center_map, kps_reg_map = infer(model, imgs)
-        imgids = imgids.numpy()
-        for b in range(params['batch_size']):
-            factor_x = widths[b].numpy() / (netW / 4)
-            factor_y = heights[b].numpy() / (netH / 4)
-            spm_decoder = SpmDecoder(factor_x, factor_y, netH // 4, netW // 4)
-            joints, centers = spm_decoder([center_map[b], kps_reg_map[b]], score_thres=score, dis_thres=dist)
-            img_id = str(imgids[b], encoding='utf-8')
+        val_dataset = get_dataset(mode='val')
+        predictions = []
 
-            predict = {}
-            predict['image_id'] = img_id
-            kps = {}
-            bbox = {}
-            human = 1
-            for j, single_person_joints in enumerate(joints):
-                joints = []
-                for i in range(14):
-                    x = int(single_person_joints[2 * i])
-                    y = int(single_person_joints[2 * i + 1])
-                    v = 1
-                    joints += [x, y, v]
-                kps['human' + str(human)] = joints
-                # bbox['human' + str(human)] = None
-                human += 1
-            predict['keypoint_annotations'] = kps
-            predictions.append(predict)
-        e = time.time()
-        print("processing.... {} / {}, time cost == {}".format(step, 30000 // params['batch_size'], e-s))
+        for step, (imgids, heights, widths, imgs) in enumerate(val_dataset):
+            s = time.time()
+            center_map, kps_reg_map = infer(model, imgs)
+            imgids = imgids.numpy()
+            for b in range(params['batch_size']):
+                factor_x = widths[b].numpy() / (netW / 4)
+                factor_y = heights[b].numpy() / (netH / 4)
+                spm_decoder = SpmDecoder(factor_x, factor_y, netH // 4, netW // 4)
+                joints, centers = spm_decoder([center_map[b], kps_reg_map[b]], score_thres=score, dis_thres=dist)
+                img_id = str(imgids[b], encoding='utf-8')
 
-    print(len(predictions))
-    with open('jsons/' + ckpt_path.split('/')[-1] + '_predicts.json', 'w') as fw:
-        json.dump(predictions, fw)
-        print('done')
+                predict = {}
+                predict['image_id'] = img_id
+                kps = {}
+                bbox = {}
+                human = 1
+                for j, single_person_joints in enumerate(joints):
+                    joints = []
+                    for i in range(14):
+                        x = int(single_person_joints[2 * i])
+                        y = int(single_person_joints[2 * i + 1])
+                        v = 1
+                        joints += [x, y, v]
+                    kps['human' + str(human)] = joints
+                    # bbox['human' + str(human)] = None
+                    human += 1
+                predict['keypoint_annotations'] = kps
+                predictions.append(predict)
+            e = time.time()
+            print("processing.... {} / {}, time cost == {}".format(step, 30000 // params['batch_size'], e-s))
 
+        print(len(predictions))
+        if 'keras' in ckpt_path:
+            json_file = 'jsons/keras/ckpt_16_finetune/' + str(ckpt) + '_predicts.json'
+        else:
+            json_file = 'jsons/' + str(ckpt) + '_predicts.json'
 
-            #### test
-            # factor_x = 4
-            # factor_y = 4
-            # spm_decoder = SpmDecoder(factor_x, factor_y, netH // 4, netW // 4)
-            # joints, centers = spm_decoder([center_map[b], kps_reg_map[b]], score_thres=score, dis_thres=dist)
-            # img_ori = (imgs[b].numpy() * 255).astype(np.uint8)
-            # for j, single_person_joints in enumerate(joints):
-            #     cv2.circle(img_ori, (int(centers[j][0]), int(centers[j][1])), 8, colors[j % 6], thickness=-1)
-            #     for i in range(14):
-            #         x = int(single_person_joints[2 * i])
-            #         y = int(single_person_joints[2 * i + 1])
-            #         cv2.circle(img_ori, (x, y), 4, colors[j % 6], thickness=-1)
-            #         cv2.putText(img_ori, str(i), (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 250), 1)
-            # cv2.imshow('batch_%d' % b, img_ori)
-            # k = cv2.waitKey(0)
-            # if k == 113:
-            #     break
-            ####
+        with open(json_file, 'w') as fw:
+            json.dump(predictions, fw)
+            print(ckpt, ' is done.')
+
